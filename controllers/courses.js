f460d92cfa676ac43a230d5571177211d3ece18b
ArqamRafay <getarqamnow@gmail.com>
2020-08-26 03:04:33 -0700

    full crud operation perform

--- controllers/courses.js
+++ controllers/courses.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const CourseModel = mongoose.model("Course")
 
 router.get("/list", (req, res) => {
+    // select * from tblCourse
     CourseModel.find((error, docs) => {
         if (!error) {
             // console.log(docs)
@@ -12,12 +13,24 @@ router.get("/list", (req, res) => {
             res.send('Error')
         }
     })
+
+    // select * from tblCourse where CourseId
+    // let query = { CourseId: "3" }
+    // CourseModel.find(query, (error, docs) => {
+    //     if (!error) {
+    //         // console.log(docs)
+    //         res.send(docs)
+    //     } else {
+    //         res.send('Error')
+    //     }
+    // })
+
 })
 
 router.post("/add", (req, res) => {
 
+    // insert into tblCourse(columnname) values ('')
     console.log(req.body);
-
     var course = new CourseModel();
     course.CourseId = req.body['CourseId']
     course.CourseName = req.body['CourseName'];
@@ -35,4 +48,32 @@ router.post("/add", (req, res) => {
 
 })
 
+router.put("/edit", (req, res) => {
+    console.log(req.body)
+    // update tblCourse set column Name = '' wheer CoirseId = 1
+    let query = { CourseId: "5" }
+    var newvalues = { $set: { CourseName: "Mickey" } };
+
+    CourseModel.updateOne(query, newvalues, (error, docs) => {
+        if (!error) {
+            res.send(docs)
+        } else {
+            res.send('Error')
+        }
+    })
+
+})
+
+router.delete("/delete", (req, res) => {
+    // console.log(req.body['CourseId'])
+    // update tblCourse set column Name = '' wheer CoirseId = 1
+    let query = { CourseId: "3" }
+    CourseModel.deleteOne(query, (error, docs) => {
+        if (!error) {
+            res.send(docs)
+        } else {
+            res.send('Error')
+        }
+    })
+})
 module.exports = router;
